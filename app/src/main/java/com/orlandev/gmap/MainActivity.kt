42c4b5c1f2659abf949f3be2e65cbc8154822168
orlandev <dfashion.corp@gmail.com>
2022-03-03 14:25:30 -0600

    Delete unused variables

--- app/src/main/java/com/orlandev/gmap/MainActivity.kt
+++ app/src/main/java/com/orlandev/gmap/MainActivity.kt
@@ -25,8 +25,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val description =
-                        "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen."
                     val listOfMapPoints = listOf<MapPlaceInfo>(
                         MapPlaceInfo(
                             location = MapPoint(20.009806, -75.828503),
@@ -72,7 +70,16 @@ class MainActivity : ComponentActivity() {
                         )
                     MapScreen(
                         listOfMapPoints = listOfMapPoints,
-                        listOfFilters = listOf( "First", "Second", "Third", "Four","Six","Seven","Home","Other")
+                        listOfFilters = listOf(
+                            "First",
+                            "Second",
+                            "Third",
+                            "Four",
+                            "Six",
+                            "Seven",
+                            "Home",
+                            "Other"
+                        )
                     ) {
                         LazyColumn() {
                             items(1000) {
