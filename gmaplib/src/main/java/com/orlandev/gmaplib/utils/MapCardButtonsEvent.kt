6f9180638117b39363cba9edf3713e60ebfd9aac
Orlando Novas Rodriguez <onovas05@gmail.com>
2022-03-11 16:10:30 -0500

    🔥 Delete unused class created for events because is not used any more...

--- gmaplib/src/main/java/com/orlandev/gmaplib/utils/MapCardButtonsEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.orlandev.gmaplib.utils
-
-import com.orlandev.gmaplib.model.MapPlaceInfo
-
-sealed class MapCardButtonsEvent() {
-    data class OnShareEvent(val mapPlaceInfo: MapPlaceInfo) : MapCardButtonsEvent()
-    data class OnSeeMoreEvent(val mapPlaceInfo: MapPlaceInfo) : MapCardButtonsEvent()
-    data class OnGoUrlEvent(val mapPlaceInfo: MapPlaceInfo) : MapCardButtonsEvent()
-}
